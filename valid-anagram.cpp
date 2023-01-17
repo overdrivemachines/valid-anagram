76afaa63a8abb6b693d9040dbcadad7bba556096
get.dipen@gmail.com <get.dipen@gmail.com>
2023-01-16 18:12:21 -0800

    completed

--- valid-anagram.cpp
+++ valid-anagram.cpp
@@ -1,7 +1,30 @@
 #include <iostream>
+#include <map>
 
 using namespace std;
 
+bool isAnagram(string s, string t) {
+  map<char, int> letters;
+
+  if (s.length() != t.length())
+    return false;
+  for (auto c: s) {
+    letters[c]++;
+  }
+
+  for (auto c: t) {
+    letters[c]--;
+    if (letters[c] < 0)
+      return false;
+  }
+
+  return true;
+}
+
 int main(int argc, char const *argv[]) {
+
+  cout << isAnagram("anagram", "nagaram") << "\n";
+  cout << isAnagram("rat", "car") << "\n";
+
   return 0;
 }
